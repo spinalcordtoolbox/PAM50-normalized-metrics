8fb2d7ee0106300d7fd05e192dbb0088202508d0
valosekj <jan.valosek@upol.cz>
2023-09-04 17:47:29 -0400

    Make function for plotting the normative values table universal for the whole cohort and also for F and M

--- notebooks/tables.py
+++ notebooks/tables.py
@@ -48,7 +48,7 @@ def table_demographic_characteristics(path_csv):
     fig.show()
 
 
-def table_whole_cohort(path_csv):
+def table_normative_values(path_csv):
     """
     Show table from csv file
     Args:
@@ -92,52 +92,3 @@ def table_whole_cohort(path_csv):
 
     fig.show()
 
-
-def table_persex(path_csv):
-    """
-    Show table from csv file
-    Args:
-        path_csv:
-    """
-
-    header_values = ['',
-                     'Axial Slice #',
-                     'CSA [mm²]',
-                     '',
-                     'AP Diameter [mm]',
-                     '',
-                     'RL Diameter [mm]',
-                     '',
-                     'Compression Ratio [a.u.]',
-                     '',
-                     'Eccentricity [a.u.]',
-                     '',
-                     'Solidity [%]']
-
-    df = pd.read_csv(path_csv)
-
-    # Add HTML <b> tags to make the header text bold
-    header_values = ['<b>' + val + '</b>' if val != '' else '' for val in header_values]
-
-    # Define the font and fill colors
-    font_style = FONT_STYLE
-
-    # Calculate the height based on the number of rows
-    table_height = 40 + len(df) * 40  # Assuming 40px height per row
-
-    fig = go.Figure(data=[go.Table(
-        header=dict(values=header_values,
-                    align=['left', 'center'],
-                    fill_color='grey',
-                    font=font_style),
-        cells=dict(values=df.transpose().values.tolist(),
-                   align=['left', 'center'],
-                   fill_color=[['white', 'lightgrey'] * (len(df) + 1)],
-                   font=font_style))
-    ])
-
-    fig.update_layout(
-        margin=FIGURE_MARGIN,
-        height=table_height)
-
-    fig.show()
