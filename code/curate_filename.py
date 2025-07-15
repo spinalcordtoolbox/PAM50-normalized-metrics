8b9b8da37df08d78bb281e49417de3d43c7b2332
Sandrine Bedard <sandrine.bedard@polymtl.ca>
2025-07-15 11:50:58 -0400

    filter with participants.tsv and curate to reduce path

--- code/curate_filename.py
+++ code/curate_filename.py
@@ -8,10 +8,11 @@ def process_filename(original_path):
     Extracts and modifies the filename path based on given rules.
     Specifically, from the absolute input path, keep only `sub-XXX/anat/sub-XXX_T2w_label-SC_seg.nii.gz`.
     """
-    match = re.search(r"(sub-[^/]+/anat/sub-[^/]+)_T2w_seg-manual\.nii\.gz$", original_path)
+    processed_path = os.path.join(*original_path.split("/")[-3::]) 
+    match = re.search(r"(sub-[^/]+/anat/sub-[^/]+)_T2w_seg-manual\.nii\.gz$", processed_path)
     if match:
         return f"{match.group(1)}_T2w_label-SC_seg.nii.gz"
-    return original_path  # Return unchanged if it doesn't match the expected format
+    return processed_path  # Return unchanged if it doesn't match the expected format
 
 def process_csv(input_path, output_folder):
     """Processes a single CSV file, modifying relevant filename columns and saving in the output folder."""
@@ -32,16 +33,24 @@ def main():
     parser = argparse.ArgumentParser(description="Modify filenames in CSV files.")
     parser.add_argument("-folder", type=str, help="Folder containing CSV files", required=True)
     parser.add_argument("-ofolder", type=str, help="Folder to save modified CSV files", required=True)
+    parser.add_argument("-participants", type=str, help="Participants.tsv to only inlcude participants with", required=True)
     args = parser.parse_args()
 
     folder = args.folder
-
+    participants_file = args.participants
     if not os.path.isdir(folder):
         print(f"Error: Folder '{folder}' does not exist.")
         return
-
+    if not os.path.isfile(participants_file):
+        print(f"Error: Participants file '{participants_file}' does not exist.")
+        return
+    participants_df = pd.read_csv(participants_file, sep="\t")
     csv_files = [f for f in os.listdir(folder) if f.endswith(".csv")]
-
+    participant_ids = set(participants_df['participant_id'].astype(str))
+    csv_files = [
+        f for f in csv_files
+        if any(pid in f for pid in participant_ids)
+    ]
     if not csv_files:
         print("No CSV files found in the folder.")
         return
